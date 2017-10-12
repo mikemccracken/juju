09a0e916a91523a3204a4ae5df0642fd551fef29
Michael McCracken <mike.mccracken@canonical.com>
2017-10-12 21:15:27 +0000

    Add CAASModel analog to IAASModel
    Signed-off-by: Michael McCracken <mike.mccracken@canonical.com>

---
+++ state/caasmodel.go
@@ -0,0 +1,49 @@
+// Copyright 2017 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package state
+
+import "github.com/juju/errors"
+
+// CAASModel contains functionality that is specific to an
+// Containers-As-A-Service (CAAS) model. It embeds a Model so that
+// all generic Model functionality is also available.
+type CAASModel struct {
+	*Model
+
+	mb modelBackend
+
+	// TODO(caas): This should be removed once things
+	// have been sufficiently untangled.
+	st *State
+}
+
+// CAASModel returns an Containers-As-A-Service (CAAS) model.
+func (m *Model) CAASModel() (*CAASModel, error) {
+	if m.Type() != ModelTypeCAAS {
+		return nil, errors.NotSupportedf("called CAASModel() on a non-CAAS Model")
+	}
+	return &CAASModel{
+		Model: m,
+		mb:    m.st,
+		st:    m.st,
+	}, nil
+}
+
+// CAASModel returns Containers-As-A-Service (CAAS) model.
+//
+// TODO(caas): This is a convenience helper only and will go away
+// once most model related functionality has been moved from State to
+// Model/CAASModel. Model.CAASModel() should be preferred where-ever
+// possible.
+func (st *State) CAASModel() (*CAASModel, error) {
+	m, err := st.Model()
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	im, err := m.CAASModel()
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+	return im, nil
+}
